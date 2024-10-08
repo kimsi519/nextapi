d90e0e6b6d9a4a87e152065d13d9a62d1ab18e1a
kimsi519 <tlsdlf5@gmail.com>
2024-10-08 15:09:46 +0900

    2

--- app/api/books/[bookId]/route.ts
+++ app/api/books/[bookId]/route.ts
@@ -1,28 +1,27 @@
-// /app/api/books/[bookId]/route.ts
-
 import { NextRequest, NextResponse } from "next/server";
-import { redirect } from "next/navigation"; // 리디렉션을 처리하기 위한 모듈
-import { books } from "../../../data/bookdata"; // 도서 데이터를 가져옴
+import { books } from "../../../data/bookdata";
 
-type Params = {
-  params: {
-    bookId: string;
-  };
-};
-
-const getBook = (bookId: string) => books.find((book) => book.id === +bookId);
-
-export async function GET(
-  _request: NextRequest,
-  { params: { bookId } }: Params
+// PATCH 요청 처리
+export async function PATCH(
+  request: NextRequest,
+  { params: { bookId } }: { params: { bookId: string } }
 ) {
-  const book = getBook(bookId);
+  const { title } = await request.json();
+  const book = books.find((book) => book.id === +bookId);
 
   if (!book) {
-    return redirect("/api/books"); // return 추가
+    return NextResponse.json({ error: "Book not found" }, { status: 404 });
   }
 
+  book.title = title;
   return NextResponse.json(book);
 }
 
-export const dynamic = "force-dynamic";
+export async function DELETE(
+  request: NextRequest,
+  { params: { bookId } }: { params: { bookId: string } }
+) {
+  const bookIndex = books.findIndex((book) => book.id === +bookId);
+  books.splice(bookIndex, 1);
+  return NextResponse.json(books);
+}
